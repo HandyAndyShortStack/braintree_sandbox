1d43e26d917a903c2f8cc5be9f6d08a859d1b94d
Andy Short <andynshort@gmail.com>
2013-09-04 10:46:44 -0700

    empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BraintreeSandbox::Application.config.secret_token = '4530130048028cb902eb136077d5f5335b3ad7af2fb47aa589a22e37ba66f28c76e7348d671c171a72171b65d0773e31620faecd6d7d0e204bc78b80326ea1db'
